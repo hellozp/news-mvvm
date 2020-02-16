b7724e6da83287c7e3a26f3b6f8f9271ebf55788
Well <13520890831@163.com>
2020-02-16 16:37:49 +0800

    格式化代码

--- NewsDemo/app/src/main/java/com/zp/newsdemo/MainActivity.java
+++ NewsDemo/app/src/main/java/com/zp/newsdemo/MainActivity.java
@@ -1,18 +1,14 @@
 package com.zp.newsdemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModel;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
